872bdcc54e67b954193a094be0a86b2ed54c4252
Sidnei Ramos <sidneiramos02@gmail.com>
2020-10-11 22:21:36 -0300

    Requisito 1

--- desafio1.sql
+++ desafio1.sql
@@ -8,7 +8,7 @@ CREATE TABLE `planos`(
     `plano_id` INT UNIQUE AUTO_INCREMENT NOT NULL,
     `nome_plano` VARCHAR(100) NOT NULL,
     `valor` DECIMAL(5,2) NOT NULL,
-    CONSTRAINT `pk_plano_id` PRIMARY KEY (`plano_id`)
+    PRIMARY KEY (`plano_id`)
 ) engine = InnoDB;
 
 CREATE TABLE `usuarios`(
@@ -16,53 +16,46 @@ CREATE TABLE `usuarios`(
     `nome_usuario` VARCHAR(100) NOT NULL,
     `idade` INT NOT NULL,
     `plano_id` INT NOT NULL,
-    CONSTRAINT `pk_usuario_id` PRIMARY KEY (`usuario_id`),
+    PRIMARY KEY (`usuario_id`),
     CONSTRAINT `fk_plano_id_usuario` FOREIGN KEY (`plano_id`) REFERENCES `planos` (`plano_id`)
 ) engine = InnoDB;
 
 CREATE TABLE `artistas`(
     `artista_id` INT UNIQUE AUTO_INCREMENT NOT NULL,
     `nome_artista` VARCHAR(100) NOT NULL,
-    CONSTRAINT `pk_artista_id` PRIMARY KEY (`artista_id`)
+    PRIMARY KEY (`artista_id`)
 ) engine = InnoDB;
 
 CREATE TABLE `albuns`(
     `album_id` INT UNIQUE AUTO_INCREMENT NOT NULL,
     `nome_album` VARCHAR(100) NOT NULL,
-    CONSTRAINT `pk_album_id` PRIMARY KEY (`album_id`)
+    `artista_id` INT NOT NULL,
+    PRIMARY KEY (`album_id`),
+    CONSTRAINT `fk_artista_id_album` FOREIGN KEY (`artista_id`) REFERENCES `artistas` (`artista_id`)
 ) engine = InnoDB;
 
 CREATE TABLE `cancoes`(
     `cancao_id` INT UNIQUE AUTO_INCREMENT NOT NULL,
     `cancao_nome` VARCHAR(100) NOT NULL,
     `album_id` INT NOT NULL,
-    CONSTRAINT `pk_cancao_id` PRIMARY KEY (`cancao_id`),
+    PRIMARY KEY (`cancao_id`),
     CONSTRAINT `fk_album_id_cancao` FOREIGN KEY (`album_id`) REFERENCES `albuns` (`album_id`)
 ) engine = InnoDB;
 
-CREATE TABLE `artista_cancao`(
-    `artista_cancao_id` INT UNIQUE AUTO_INCREMENT NOT NULL,
-    `artista_id` INT NOT NULL,
-    `cancao_id` INT NOT NULL,
-    CONSTRAINT `pk_artista_cancao_id` PRIMARY KEY (`artista_cancao_id`),
-    CONSTRAINT `fk_artista_id_cancao` FOREIGN KEY (`artista_id`) REFERENCES `artistas` (`artista_id`),
-    CONSTRAINT `fk_cancao_id_artista` FOREIGN KEY (`cancao_id`) REFERENCES `cancoes` (`cancao_id`)
-) engine = InnoDB;
-
 CREATE TABLE `seguindo_artista`(
-    `seguindo_id` INT UNIQUE AUTO_INCREMENT NOT NULL,
+  --  `seguindo_id` INT UNIQUE AUTO_INCREMENT NOT NULL,
     `usuario_id` INT NOT NULL,
     `artista_id` INT NOT NULL,
-    CONSTRAINT `pk_seguindo_id` PRIMARY KEY (`seguindo_id`),
+	PRIMARY KEY (`usuario_id`, `artista_id`),
     CONSTRAINT `fk_usuario_id_artista` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`usuario_id`),
     CONSTRAINT `fk_artista_id_usuario` FOREIGN KEY (`artista_id`) REFERENCES `artistas` (`artista_id`)
 ) engine = InnoDB;
 
 CREATE TABLE `reproducao`(
-    `reproducao_id` INT UNIQUE AUTO_INCREMENT NOT NULL,
+   -- `reproducao_id` INT UNIQUE AUTO_INCREMENT NOT NULL,
     `usuario_id` INT NOT NULL,
     `cancao_id` INT NOT NULL,
-    CONSTRAINT `pk_reproducao_id` PRIMARY KEY (`reproducao_id`),
+    PRIMARY KEY (`usuario_id`, `cancao_id`),
     CONSTRAINT `fk_usuario_id_cancao` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`usuario_id`),
     CONSTRAINT `fk_cancao_id_usuario` FOREIGN KEY (`cancao_id`) REFERENCES `cancoes` (`cancao_id`)
 ) engine = InnoDB;
@@ -87,13 +80,13 @@ VALUES
 ('Lance Day'),
 ('Freedie Shannon');
 
-INSERT INTO albuns (nome_album)
+INSERT INTO albuns (nome_album, artista_id)
 VALUES
-('Envious'),
-('Exuberant'),
-('Hallowed Steam'),
-('Incandescent'),
-('Temporary Culture');
+('Envious', 1),
+('Exuberant', 1),
+('Hallowed Steam', 2),
+('Incandescent', 3),
+('Temporary Culture', 4);
 
 INSERT INTO cancoes (cancao_nome, album_id)
 VALUES
@@ -115,27 +108,6 @@ VALUES
 ('Thang Of Thunder', 5),
 ('Words OF Her Life', 5),
 ('Without My Streets', 5);
-
-INSERT INTO artista_cancao (artista_id, cancao_id)
-VALUES
-(1, 1),
-(1, 2),
-(1, 3),
-(1, 4),
-(1, 5),
-(2, 6),
-(2, 7),
-(2, 8),
-(2, 9),
-(3, 10),
-(3, 11),
-(3, 12),
-(3, 13),
-(3, 14),
-(3, 15),
-(4, 16),
-(4, 17),
-(4, 18);
 
 INSERT INTO seguindo_artista (usuario_id, artista_id)
 VALUES
