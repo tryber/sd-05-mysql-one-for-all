5a1f789c8eb00ba7f3c51fdab4e86a3ef7bf2616
rfreitasbatista <rfreitasbatista@gmail.com>
2020-10-20 20:04:20 -0300

    Requisito 4 issue

--- desafio4.sql
+++ desafio4.sql
@@ -4,4 +4,6 @@ A.artista_nome AS artista,
 COUNT(S.artista_id) AS seguidores
 FROM SpotifyClone.artistas AS A
 INNER JOIN SpotifyClone.seguidores AS S ON S.artista_id = A.artista_id
-GROUP BY artista;
+GROUP BY artista
+ORDER BY seguidores DESC
+LIMIT 3;
