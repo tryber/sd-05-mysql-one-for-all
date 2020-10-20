95ba5a218b6470b1bdd106ca3f86d4cc9bdf64f1
rfreitasbatista <rfreitasbatista@gmail.com>
2020-10-20 19:04:38 -0300

    Requisito 3

---
+++ desafio3.sql
@@ -0,0 +1,10 @@
+USE spotifyclone;
+
+CREATE VIEW historico_reproducao_usuarios AS
+SELECT 
+U.usuario_nome usuario,
+M.musica_nome nome
+FROM spotifyclone.usuarios AS U
+INNER JOIN spotifyclone.historico_de_reproducoes AS H ON H.usuario_id = U.usuario_id
+INNER JOIN spotifyclone.musicas AS M ON M.musica_id = H.musica_id
+ORDER BY usuario, nome;
