2ee4cba33eba2b20afb7fbe5cf0aa4dae1a55d53
Paulo-Dandrea <prog.dandrea@gmail.com>
2020-10-20 16:15:02 -0300

    cccc

--- desafio8.sql
+++ desafio8.sql
@@ -1,5 +1,3 @@
--- Crie uma trigger chamada trigger_usuario_delete que deve ser disparada sempre que uma pessoa usuária for excluída do banco de dados, 
--- refletindo essa exclusão em todas as tabelas que ela estiver.
 DELIMITER $$
 CREATE TRIGGER trigger_usuario_delete
 BEFORE DELETE ON SpotifyClone.usuario
