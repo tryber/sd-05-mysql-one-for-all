4a2944f3957763edc3b537045a6eb5c13b5acc36
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-10-20 18:18:54 -0300

    fixed order of table creation

--- desafio1.sql
+++ desafio1.sql
@@ -2,13 +2,19 @@
 -- 1) CRIAR BANCO DE DADOS
 
 DROP DATABASE IF EXISTS SpotifyClone;
-
 CREATE DATABASE SpotifyClone;
+
+-- 2) CRIAR TABELAS COM SUAS COLUNAS E CONSTRAINTS
+-- Cuidado que ordem importa, qqr tiver FK no final
 
 USE SpotifyClone;
 
-
--- 2) CRIAR TABELAS COM SUAS COLUNAS E CONSTRAINTS
+CREATE TABLE planos (
+    plano_id INT PRIMARY KEY AUTO_INCREMENT,
+    plano_nome VARCHAR(50) NOT NULL,
+    plano_valor DECIMAL(3,2) NOT NULL, -- qqr valor com 3 digitos e 2 decimais, entre -9.99 e 9.99
+    -- ref. https://dev.mysql.com/doc/refman/8.0/en/fixed-point-types.html
+) engine = InnoDB;
 
 CREATE TABLE usuarios (
   -- também podemos escrever os nomes de tabelas e colunas com crases (`usuarios`, etc)
@@ -20,26 +26,17 @@ CREATE TABLE usuarios (
     FOREIGN KEY (plano_id) REFERENCES planos(plano_id)
 ) engine = InnoDB; -- linha dada no repositorio do projeto
 
-CREATE TABLE planos (
-    plano_id INT PRIMARY KEY AUTO_INCREMENT,
-    plano_nome VARCHAR(50) NOT NULL,
-    plano_valor DECIMAL(3,2) NOT NULL, -- qqr valor com 3 digitos e 2 decimais, entre -9.99 e 9.99
-    -- ref. https://dev.mysql.com/doc/refman/8.0/en/fixed-point-types.html
-) engine = InnoDB;
-
 CREATE TABLE artistas (
     artista_id INT PRIMARY KEY AUTO_INCREMENT,
     -- todos ids no caso podiam ser também SMALLINT e TINYINT, sendo números baixos
     artista_nome VARCHAR(50) UNIQUE NOT NULL
 ) ENGINE = InnoDB;
 
-CREATE TABLE seguindo_artistas (
-    usuario_id NOT NULL,
-    artista_id NOT NULL,
-    PRIMARY KEY (usuario_id, artista_id)
-    FOREIGN KEY (usuario_id) REFERENCES usuarios(usuario_id),
+CREATE TABLE albuns (
+    album_id INT PRIMARY KEY AUTO_INCREMENT,
+    album_titulo VARCHAR(100) NOT NULL,
+    artista_id INT NOT NULL,
     FOREIGN KEY (artista_id) REFERENCES artistas(artista_id)
-    -- CONSTRAINT nome_fk é um code que pode ser inserido no começo dessas linhas de FK
 ) engine = InnoDB;
 
 CREATE TABLE cancoes (
@@ -47,13 +44,6 @@ CREATE TABLE cancoes (
     cancao_titulo VARCHAR(100) NOT NULL, -- mais carateres para titulos do que para nomes
     album_id INT NOT NULL,
     FOREIGN KEY (album_id) REFERENCES albuns(album_id)
-) engine = InnoDB;
-
-CREATE TABLE albuns (
-    album_id INT PRIMARY KEY AUTO_INCREMENT,
-    album_titulo VARCHAR(100) NOT NULL,
-    artista_id INT NOT NULL,
-    FOREIGN KEY (artista_id) REFERENCES artistas(artista_id)
 ) engine = InnoDB;
 
 CREATE TABLE historico_de_reproducoes (
@@ -64,28 +54,29 @@ CREATE TABLE historico_de_reproducoes (
     FOREIGN KEY (cancao_id) REFERENCES cancoes(cancao_id)
 ) engine = InnoDB;
 
+CREATE TABLE seguindo_artistas (
+    usuario_id NOT NULL,
+    artista_id NOT NULL,
+    PRIMARY KEY (usuario_id, artista_id)
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(usuario_id),
+    FOREIGN KEY (artista_id) REFERENCES artistas(artista_id)
+    -- CONSTRAINT nome_fk é um code que pode ser inserido no começo dessas linhas de FK
+) engine = InnoDB;
+
 
 -- 3) INSERIR VALORES NAS TABELAS
-
-INSERT INTO usuarios (usuario_nome, usuario_idade, plano_id)
-VALUES ('Thati', 23, 1), ('Cintia', 35, 3), ('Bill', 20, 2), ('Roger', 45, 1);
 
 INSERT INTO planos (plano_nome, plano_valor)
 VALUES ('gratuito', 0.00), ('universitário', 5.99), ('familiar', 7.99);
 
+INSERT INTO usuarios (usuario_nome, usuario_idade, plano_id)
+VALUES ('Thati', 23, 1), ('Cintia', 35, 3), ('Bill', 20, 2), ('Roger', 45, 1);
+
 INSERT INTO artistas (artista_nome)
 VALUES ('Walter Phoenix'), ('Peter Strong'), ('Lance Day'), ('Freedie Shannon');
 
-INSERT INTO seguindo_artistas (usuario_id, artista_id)
-VALUES
-(1, 1),
-(1, 3),
-(1, 4),
-(2, 1),
-(2, 3),
-(3, 1),
-(3, 2),
-(4, 4);
+INSERT INTO albuns (album_titulo, artista_id)
+VALUES ('Envious', 1), ('Exuberant', 1), ('Hallowed Steam', 2), ('Incandescent', 3), ('Temporary Culture', 4);
 
 INSERT INTO cancoes (cancao_titulo, album_id)
 VALUES
@@ -108,9 +99,6 @@ VALUES
 ('Words Of Her Life', 5),
 ('Without My Streets', 5);
 
-INSERT INTO albuns (album_titulo, artista_id)
-VALUES ('Envious', 1), ('Exuberant', 1), ('Hallowed Steam', 2), ('Incandescent', 3), ('Temporary Culture', 4);
-
 INSERT INTO historico_de_reproducoes (usuario_id, cancao_id)
 VALUES
 (1, 1),
@@ -127,3 +115,14 @@ VALUES
 (4, 3),
 (4, 18),
 (4, 11);
+
+INSERT INTO seguindo_artistas (usuario_id, artista_id)
+VALUES
+(1, 1),
+(1, 3),
+(1, 4),
+(2, 1),
+(2, 3),
+(3, 1),
+(3, 2),
+(4, 4);
