93aa900f8438f4a43b4d382cf3792b3ca396a608
Douglas <douglaza@gmail.com>
2020-10-20 20:26:16 -0300

    CC fixed

--- desafio10.sql
+++ desafio10.sql
@@ -6,8 +6,8 @@ RETURNS INT READS SQL DATA
 BEGIN
     DECLARE qtde INT;    
     SELECT 
-	COUNT(id_cancao)
-	FROM historico
+COUNT(id_cancao)
+FROM historico
     WHERE id_usuario = cliente INTO qtde;
     RETURN qtde;
 END $$
--- desafio8.sql
+++ desafio8.sql
@@ -3,6 +3,5 @@ CREATE TRIGGER trigger_usuario_delete
     AFTER DELETE ON usuarios
     FOR EACH ROW
 BEGIN
-	
 END; $$
 DELIMITER ;
