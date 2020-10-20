18cb249c9ddfa1bd3d06a7cb6a91384a358fcedb
Sidnei Ramos <sidneiramos02@gmail.com>
2020-10-20 18:16:59 -0300

    Refatorando requisito 10 pra ficar bunitin

--- desafio10.sql
+++ desafio10.sql
@@ -1,14 +1,12 @@
 DELIMITER $$
 
-CREATE FUNCTION quantidade_musicas_no_historico(usuar VARCHAR(50))
+CREATE FUNCTION quantidade_musicas_no_historico(usuar INT)
 RETURNS INT READS SQL DATA
 BEGIN
 DECLARE reprod INT;
 SELECT COUNT(r.cancao_id)
 FROM SpotifyClone.reproducao AS r
-INNER JOIN SpotifyClone.usuarios AS u
-ON r.usuario_id = u.usuario_id
-WHERE u.usuario_id = usuar
+WHERE r.usuario_id = usuar
 INTO reprod;
 RETURN reprod;
 END $$
