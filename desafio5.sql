11ff68b66f6dc1600978508c944fe1e9fb9cdc71
Andrevlc <andrevezi@gmail.com>
2020-10-20 20:31:27 -0300

    Desafio 5 fixed

--- desafio5.sql
+++ desafio5.sql
@@ -1,4 +1,4 @@
 CREATE VIEW top_2_hits_do_momento AS
 SELECT S.song_name AS cancao , COUNT(LH.song_id) as reproducoes FROM songs AS S
 INNER JOIN listenning_history AS LH ON S.song_id = LH.song_id
-GROUP BY cancao ORDER BY reproducoes DESC LIMIT 2;
+GROUP BY cancao ORDER BY reproducoes DESC , cancao ASC LIMIT 2;
