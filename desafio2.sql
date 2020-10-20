a2034797e5e12503ae4911cf6c474cf8985bd111
Christian Duessel <duessel@gmail.com>
2020-10-20 18:55:26 -0300

    tirando musica duplicada do desafio1

--- desafio2.sql
+++ desafio2.sql
@@ -1,6 +1,6 @@
 CREATE VIEW estatisticas_musicais AS
 SELECT
-COUNT(id) AS cancoes,
+COUNT(s.id) AS cancoes,
 (SELECT COUNT(a.id) FROM SpotifyClone.artist as a) AS artistas,
 (SELECT COUNT(al.id) FROM SpotifyClone.album as al) AS albuns
-FROM SpotifyClone.songs;
+FROM SpotifyClone.songs as s;
