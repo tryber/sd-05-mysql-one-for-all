938f3f9bdc7354e81b99a0c2061559c26876d74f
rfreitasbatista <rfreitasbatista@gmail.com>
2020-10-20 20:58:10 -0300

    Issue try

--- desafio6.sql
+++ desafio6.sql
@@ -4,5 +4,5 @@ MIN(P.valor) AS faturamento_minimo,
 MAX(P.valor) AS faturamento_maximo,
 ROUND((AVG(P.valor)), 2) AS faturamento_medio,
 SUM(P.valor) AS faturamento_total
-FROM SpotifyClone.planos AS P
-INNER JOIN SpotifyClone.usuarios AS U ON U.plano_id = P.plano_id;
+FROM SpotifyClone.usuarios AS U
+INNER JOIN SpotifyClone.planos AS P ON U.plano_id = P.plano_id;
