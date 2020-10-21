57dded1a1c2a69139817016eaf5473992ec7d3bd
RenataNovais <rnovaiscs@gmail.com>
2020-10-20 22:21:50 -0300

    removendo use bd para passar no teste

--- desafio2.sql
+++ desafio2.sql
@@ -1,7 +1,8 @@
-USE SpotifyClone;
+-- USE SpotifyClone;
 
 CREATE VIEW estatisticas_musicais AS
-SELECT COUNT(DISTINCT m.music_name) AS 'cancoes',
+SELECT 
+COUNT(DISTINCT m.music_name) AS 'cancoes',
 COUNT(DISTINCT art.artist_name) AS 'artistas',
 COUNT(DISTINCT alb.album_name) AS 'albuns'
 FROM SpotifyClone.musics AS m, SpotifyClone.artists AS art, SpotifyClone.albums AS alb;
