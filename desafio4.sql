ae7b467c24603aafa013c8e2e2253faf8456ed0f
RenataNovais <rnovaiscs@gmail.com>
2020-10-20 23:12:12 -0300

    desafio 4

---
+++ desafio4.sql
@@ -0,0 +1,11 @@
+-- USE SpotifyClone;
+-- SELECT * FROM top_3_artistas;
+
+CREATE VIEW top_3_artistas AS
+SELECT
+a.artist_name AS 'artista',
+COUNT(f.user_id) AS 'seguidores'
+FROM SpotifyClone.artists AS a
+INNER JOIN SpotifyClone.followers AS f ON f.artist_id = a.id
+GROUP BY artist_id
+ORDER BY 2 DESC, 1;
