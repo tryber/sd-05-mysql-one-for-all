5bc6a2451cd53c60099c6f1af9977ef0b41296c8
Jorge Lucas <jorge.lucas2264@gmail.com>
2020-10-21 09:33:47 -0300

    desafio 3 completo

---
+++ desafio3.sql
@@ -0,0 +1,7 @@
+create view historico_reproducao_usuarios as
+select usuario, cancao from historico_de_reproducoes
+inner join usuarios
+on usuarios.usuarioId = historico_de_reproducoes.usuario_id
+inner join cancoes
+on cancoes.cancaoId = historico_de_reproducoes.cancao_id
+order by usuario, cancao;
