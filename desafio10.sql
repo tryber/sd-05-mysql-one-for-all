48d8f56dd4c13f6240500d700de08398b67b43f8
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-10-21 12:26:50 -0300

    challenge 10 function

---
+++ desafio10.sql
@@ -0,0 +1,21 @@
+-- Crie uma function chamada de quantidade_musicas_no_historico que exibe a quantidade de músicas
+-- que estão presentes atualmente no histórico de reprodução de uma pessoa usuária.
+-- Ao receber o código identificador da pessoa,
+-- exiba a quantidade de canções em seu histórico de reprodução.
+
+USE SpotifyClone;
+DELIMITER $$
+
+CREATE FUNCTION quantidade_musicas_no_historico(idUser INT)
+RETURNS INT READS SQL DATA
+
+BEGIN
+DECLARE numReproducoes INT;
+SELECT COUNT(cancao_id) FROM SpotifyClone.historico_de_reproducoes WHERE usuario_id = idUser
+INTO numReproducoes;
+RETURN numReproducoes;
+END $$
+
+DELIMITER ;
+
+-- SELECT quantidade_musicas_no_historico(3); -- 3 sendo o id de 'Bill', retorno certo no WorkBench
