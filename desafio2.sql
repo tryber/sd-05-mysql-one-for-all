6f737255eb46d7bd3a878f017b6904d2d1555c59
LizzardMedeiros <luis.medeiros@outlook.com.br>
2020-10-21 08:05:35 -0300

    ch02

--- desafio2.sql
+++ desafio2.sql
@@ -1,5 +1,7 @@
+USE spotifyclone;
+
 CREATE VIEW estatisticas_musicais AS
 SELECT COUNT(id) AS cancoes,
-(SELECT COUNT(id) FROM spotifyclone.artista) AS artistas,
-(SELECT COUNT(id) FROM spotifyclone.album) AS albuns
-FROM spotifyclone.cancao;
+(SELECT COUNT(id) FROM artista) AS artistas,
+(SELECT COUNT(id) FROM album) AS albuns
+FROM cancao;
