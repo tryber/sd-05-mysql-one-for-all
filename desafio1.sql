3354c44c981be914cf161f4655580fb874b09e84
hleoc <informig@gmail.com>
2020-10-20 22:15:40 -0300

    Primeiro commit - corrigindo erros do CC

--- desafio1.sql
+++ desafio1.sql
@@ -16,9 +16,9 @@ CREATE TABLE IF NOT EXISTS `SpotifyClone`.`plano` (
 ENGINE = InnoDB;
 
 INSERT INTO plano(nome_plano, valor_plano)
-	VALUES('gratuito', 0.00),
-		('familiar', 7.99),
-        ('universitário', 5.99);
+VALUES('gratuito', 0.00),
+('familiar', 7.99),
+('universitário', 5.99);
 
 -- -----------------------------------------------------
 -- Table `SpotifyClone`.`usuario`
@@ -39,11 +39,11 @@ CREATE TABLE IF NOT EXISTS `SpotifyClone`.`usuario` (
 ENGINE = InnoDB;
 
 INSERT INTO usuario(nome_usuario, idade, plano_id)
-	VALUES('Thati', 23, 1),
-    ('Cintia', 35, 2),
-    ('Bill', 20, 3),
-    ('Roger', 45, 1);
-    
+VALUES('Thati', 23, 1),
+('Cintia', 35, 2),
+('Bill', 20, 3),
+('Roger', 45, 1);
+
 -- -----------------------------------------------------
 -- Table `SpotifyClone`.`artista`
 -- -----------------------------------------------------
@@ -55,10 +55,10 @@ CREATE TABLE IF NOT EXISTS `SpotifyClone`.`artista` (
 ENGINE = InnoDB;
 
 INSERT INTO artista(nome_artista)
-	VALUE('Walter Phoenix'),
-		('Peter Strong'),
-        ('Lance Day'),
-        ('Freedie Shannon');
+VALUE('Walter Phoenix'),
+('Peter Strong'),
+('Lance Day'),
+('Freedie Shannon');
 
 -- -----------------------------------------------------
 -- Table `SpotifyClone`.`album`
@@ -78,11 +78,11 @@ CREATE TABLE IF NOT EXISTS `SpotifyClone`.`album` (
 ENGINE = InnoDB;
 
 INSERT INTO album(nome_album, artista_id)
-	VALUES('Envious', 1),
-		('Exuberant', 1),
-        ('Hallowed Steam', 2),
-        ('Incandescent', 3),
-        ('Temporary Culture', 4);
+VALUES('Envious', 1),
+('Exuberant', 1),
+('Hallowed Steam', 2),
+('Incandescent', 3),
+('Temporary Culture', 4);
 
 -- -----------------------------------------------------
 -- Table `SpotifyClone`.`cancao`
@@ -102,24 +102,24 @@ CREATE TABLE IF NOT EXISTS `SpotifyClone`.`cancao` (
 ENGINE = InnoDB;
 
 INSERT INTO cancao(nome_cancao, album_id)
-	VALUES('Soul For Us', 1),
-		('Reflections Of Magic', 1),
-        ('Dance With Her Own', 1),
-        ('Troubles Of My Inner Fire', 2),
-        ('Time Fireworks', 2),
-        ('Magic Circus', 3),
-        ('Honey, So Do I', 3),
-        ('Sweetie, Lets Go Wild', 3),
-        ('She Knows', 3),
-        ('Fantasy For Me', 4),
-        ('Celebration Of More', 4),
-        ('Rock His Everything', 4),
-        ('Home Forever', 4),
-        ('Diamond Power', 4),
-        ('Honey, Lets Be Silly', 4),
-        ('Thang Of Thunder', 5),
-        ('Words Of Her Life', 5),
-        ('Without My Streets', 5);
+VALUES('Soul For Us', 1),
+('Reflections Of Magic', 1),
+('Dance With Her Own', 1),
+('Troubles Of My Inner Fire', 2),
+('Time Fireworks', 2),
+('Magic Circus', 3),
+('Honey, So Do I', 3),
+('Sweetie, Lets Go Wild', 3),
+('She Knows', 3),
+('Fantasy For Me', 4),
+('Celebration Of More', 4),
+('Rock His Everything', 4),
+('Home Forever', 4),
+('Diamond Power', 4),
+('Honey, Lets Be Silly', 4),
+('Thang Of Thunder', 5),
+('Words Of Her Life', 5),
+('Without My Streets', 5);
 
 -- -----------------------------------------------------
 -- Table `SpotifyClone`.`usuario_seguindo_artista`
@@ -145,14 +145,14 @@ CREATE TABLE IF NOT EXISTS `SpotifyClone`.`usuario_seguindo_artista` (
 ENGINE = InnoDB;
 
 INSERT INTO usuario_seguindo_artista(usuario_id, artista_id)
-	VALUES(1, 1),
-		(1, 4),
-        (1, 3),
-        (2, 1),
-        (2, 3),
-        (3, 2),
-        (3, 1),
-        (4, 4);
+VALUES(1, 1),
+(1, 4),
+(1, 3),
+(2, 1),
+(2, 3),
+(3, 2),
+(3, 1),
+(4, 4);
 
 -- -----------------------------------------------------
 -- Table `SpotifyClone`.`historico_de_reproducoes`
@@ -178,17 +178,17 @@ CREATE TABLE IF NOT EXISTS `SpotifyClone`.`historico_de_reproducoes` (
 ENGINE = InnoDB;
 
 INSERT INTO historico_de_reproducoes(usuario_id, cancao_id)
-	VALUES(1, 1),
-		(1, 6),
-        (1, 14),
-        (1, 16),
-        (2, 13),
-        (2, 17),
-        (2, 2),
-        (2, 15),
-        (3, 4),
-        (3, 16),
-        (3, 6),
-        (4, 3),
-        (4, 18),
-        (4, 11);
+VALUES(1, 1),
+(1, 6),
+(1, 14),
+(1, 16),
+(2, 13),
+(2, 17),
+(2, 2),
+(2, 15),
+(3, 4),
+(3, 16),
+(3, 6),
+(4, 3),
+(4, 18),
+(4, 11);
