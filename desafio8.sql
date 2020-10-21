98b17095728101e72ec9c186bab91ed7ef47c9af
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-10-21 13:06:07 -0300

    fixed challenge 8 - before instead of after

--- desafio8.sql
+++ desafio8.sql
@@ -6,7 +6,7 @@
 DELIMITER $$
 
 CREATE TRIGGER trigger_usuario_delete
-AFTER DELETE ON SpotifyClone.usuarios
+BEFORE DELETE ON SpotifyClone.usuarios
 FOR EACH ROW
 BEGIN
   DELETE FROM SpotifyClone.seguindo_artistas WHERE usuario_id = OLD.usuario_id;
