d48f09fad960e606bfd0824ce083eb11cf6422e6
pedrohcalado <pedrocalado22@gmail.com>
2020-10-21 11:32:28 -0300

    Desafio8

---
+++ desafio8.sql
@@ -0,0 +1,9 @@
+DELIMITER $$
+CREATE TRIGGER trigger_usuario_delete
+    BEFORE DELETE ON usuarios
+    FOR EACH ROW
+BEGIN
+    DELETE FROM SpotifyClone.usuarios_cancoes WHERE usuario_id = OLD.id;
+    DELETE FROM SpotifyClone.usuarios_artistas WHERE usuario_id = OLD.id;
+END; $$
+DELIMITER ;
