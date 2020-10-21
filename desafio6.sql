8989ac98bfae730beb906251f64b2d2d8e1be4ac
rfreitasbatista <rfreitasbatista@gmail.com>
2020-10-20 21:11:19 -0300

    Cast test

--- desafio6.sql
+++ desafio6.sql
@@ -2,7 +2,7 @@ CREATE VIEW faturamento_atual AS
 SELECT
 MIN(P.valor) AS faturamento_minimo,
 MAX(P.valor) AS faturamento_maximo,
-ROUND(AVG(P.valor), 2) AS faturamento_medio,
+CAST(ROUND(AVG(P.valor), 2) AS DECIMAL(5,2)) faturamento_medio,
 SUM(P.valor) AS faturamento_total
 FROM SpotifyClone.usuarios AS U
 INNER JOIN SpotifyClone.planos AS P ON U.plano_id = P.plano_id;
