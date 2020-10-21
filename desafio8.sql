82ef7ecc40158209dc63a0b53832d1a7d312a01e
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-10-21 11:43:57 -0300

    fixed syntax challenge 8

--- desafio8.sql
+++ desafio8.sql
@@ -12,6 +12,6 @@ BEGIN
   DELETE FROM SpotifyClone.seguindo_artistas WHERE usuario_id = OLD.usuario_id;
   DELETE FROM SpotifyClone.historico_de_reproducoes WHERE usuario_id = OLD.usuario_id;
     -- mapeando todas as tabelas onde usuario aparecer e usando OLD, ref sintaxe e palavras chave: https://www.devmedia.com.br/triggers-pl-sql-saiba-quando-e-por-que-usar/30011
-END;
+END $$
 
-DELIMITER $$ ;
+DELIMITER ;
