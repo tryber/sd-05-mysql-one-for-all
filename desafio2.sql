f6c9c73bfb6f863a76b46089cd5339a783b9fee9
LizzardMedeiros <luis.medeiros@outlook.com.br>
2020-10-21 09:20:36 -0300

    ch03

--- desafio2.sql
+++ desafio2.sql
@@ -1,7 +1,5 @@
-USE spotifyclone;
-
 CREATE VIEW estatisticas_musicais AS
 SELECT COUNT(id) AS cancoes,
-(SELECT COUNT(id) FROM artista) AS artistas,
-(SELECT COUNT(id) FROM album) AS albuns
-FROM cancao;
+(SELECT COUNT(id) FROM spotifyclone.artista) AS artistas,
+(SELECT COUNT(id) FROM spotifyclone.album) AS albuns
+FROM spotifyclone.cancao;
--- desafio3.sql
+++ desafio3.sql
@@ -1,8 +1,8 @@
 CREATE VIEW `historico_reproducao_usuarios` AS
-SELECT U.usuario, C.cancao AS nome
+SELECT U.usuario AS usuario, C.cancao AS nome
 FROM spotifyclone.usuario AS U
 INNER JOIN spotifyclone.reproducao AS R
 ON R.usuario_id = U.id
 INNER JOIN spotifyclone.cancao AS C
 ON R.cancao_id = C.id
-ORDER BY U.usuario, nome;
+ORDER BY usuario, nome;
