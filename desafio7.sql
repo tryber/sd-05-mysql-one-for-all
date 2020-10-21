ed0f4154c8481e6158c69958e79f8959681ddfeb
rfreitasbatista <rfreitasbatista@gmail.com>
2020-10-20 22:26:40 -0300

    requisito 8

--- desafio7.sql
+++ desafio7.sql
@@ -1,8 +1,8 @@
 CREATE VIEW perfil_artistas AS
 SELECT 
-A.artista_nome artista,
-AL.album_nome album,
-COUNT(S.usuario_id) seguidores
+A.artista_nome AS artista,
+AL.album_nome AS album,
+COUNT(S.usuario_id) AS seguidores
 FROM SpotifyClone.seguidores AS S
 INNER JOIN SpotifyClone.artistas AS A ON A.artista_id = S.artista_id
 INNER JOIN SpotifyClone.albuns AS AL ON AL.artista_id = S.artista_id
