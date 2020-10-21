61e3ce6254cce0bc955aa7eb1389304ec5a29cf6
RenataNovais <rnovaiscs@gmail.com>
2020-10-20 22:33:58 -0300

    cc nao permite comentarios ao final do arq

--- desafio3.sql
+++ desafio3.sql
@@ -8,5 +8,3 @@ FROM SpotifyClone.users AS u
 INNER JOIN SpotifyClone.music_history AS mh ON u.id = mh.user_id
 INNER JOIN SpotifyClone.musics AS m ON mh.music_id = m.id
 ORDER BY 1, 2;
-
--- SELECT * FROM historico_reproducao_usuarios;
