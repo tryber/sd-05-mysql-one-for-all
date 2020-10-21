fdc839d692656b105404c7d87ab1daf1fe8e2cba
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-10-21 01:46:59 -0300

    Passando no desafio 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,6 +1,9 @@
-CREATE VIEW IF NOT EXISTS SpotifyClone.cancoes_premium AS
+CREATE VIEW SpotifyClone.cancoes_premium AS
 SELECT can.cancao_nome AS `nome`, COUNT(1) AS `reproducoes`
 FROM SpotifyClone.cancao AS can
   INNER JOIN SpotifyClone.historico_reproducao AS his ON (can.cancao_id = his.cancao_id)
-GROUP BY can.cancao_nome
+  INNER JOIN SpotifyClone.usuario AS usr ON (his.usuario_id = usr.usuario_id)
+  INNER JOIN SpotifyClone.plano AS pln ON (usr.plano_id = pln.plano_id)
+WHERE pln.plano_descricao IN ("universitário", "familiar")
+GROUP BY nome
 ORDER BY nome;
