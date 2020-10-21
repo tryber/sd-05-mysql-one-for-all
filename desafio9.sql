a6ea6b4bd8b4a1d5b6908c67bbd0c03a49e62c84
AndersonAGodoy <anderson.a.godoy@gmail.com>
2020-10-21 14:12:12 -0300

    desafio 9 feito

---
+++ desafio9.sql
@@ -0,0 +1,10 @@
+DELIMITER $$
+CREATE PROCEDURE albuns_do_artista (IN art VARCHAR(50))
+BEGIN 
+SELECT ar.artist_name AS artista,
+album_name AS album
+FROM SpotifyClone.albuns AS al
+INNER JOIN SpotifyClone.artists AS ar
+ON al.artist_id = ar.artist_id;
+END $$
+DELIMITER ;
