96fc270350bb6c4ae4e938be7d47022ec6c4150c
luiz-avelar <luiz.avelar25@gmail.com>
2020-10-21 22:29:24 -0300

    Codeclimate

--- desafio8.sql
+++ desafio8.sql
@@ -3,7 +3,7 @@ CREATE TRIGGER trigger_usuario_delete
     AFTER DELETE ON usuários
     FOR EACH ROW
 BEGIN
-    DELETE FROM seguidores WHERE seguidores.usuário = usuários.id;
-	DELETE FROM reproduções WHERE reproduções.usuário = usuários.id;
+DELETE FROM seguidores WHERE seguidores.usuário = usuários.id;
+DELETE FROM reproduções WHERE reproduções.usuário = usuários.id;
 END; $$
 DELIMITER ;
