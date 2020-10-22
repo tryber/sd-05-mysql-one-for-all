0496aff5cb6e048720ab03854e667037297d186f
luiz-avelar <luiz.avelar25@gmail.com>
2020-10-22 14:10:40 -0300

    Challenge 10 updated

--- desafio10.sql
+++ desafio10.sql
@@ -20,16 +20,16 @@ DELIMITER ;
 
 DELIMITER $$
 
-CREATE FUNCTION quantidade_musicas_no_historico(usuarioNome VARCHAR(50))
+CREATE FUNCTION quantidade_musicas_no_historico(idUser INT)
 RETURNS INT READS SQL DATA
 BEGIN
-RETURN (
-SELECT COUNT(*) AS quantidade_musicas_no_historico 
-FROM SpotifyClone.reproduções AS r
-INNER JOIN SpotifyClone.usuários AS u
-ON r.usuário = u.id
-WHERE u.nome = usuarioNome
-);
+DECLARE quantidadeMusicas INT;
+SELECT COUNT(*) INTO quantidadeMusicas
+FROM SpotifyClone.reproduções
+WHERE usuario = idUser;
+RETURN quantidadeMusicas;
+
 END $$
 
 DELIMITER ;
+
