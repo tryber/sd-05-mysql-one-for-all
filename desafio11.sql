9495be1af3345fd9b56fadeacd9b41f07f2bb3fe
ferrari <luisdiasferrari@gmail.com>
2020-10-22 18:42:41 -0300

    Desafio 11 Done

---
+++ desafio11.sql
@@ -0,0 +1,10 @@
+USE SpotifyClone;
+
+DROP VIEW IF EXISTS cancoes_premium;
+
+CREATE VIEW cancoes_premium AS
+SELECT C.cancao AS 'nome', COUNT(H.cancao_id) AS 'reproducoes'
+FROM SpotifyClone.cancoes AS C, SpotifyClone.historico AS H, SpotifyClone.usuarios AS U
+WHERE C.cancao_id = H.cancao_id AND H.usuario_id = U.usuario_id AND U.plano_id IN (2,3)
+GROUP BY C.cancao
+ORDER BY C.cancao;
