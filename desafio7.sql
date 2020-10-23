cb69237de6e4e1e4c2d7fdc7f772d28f7a355e30
Preto115 <preto115@gmail.com>
2020-10-23 10:52:58 -0300

    desafio7 implementado

---
+++ desafio7.sql
@@ -0,0 +1,19 @@
+-- desafio7
+CREATE VIEW perfil_artistas AS
+SELECT A.nome_artista AS 'artista' ,
+AL.titulo_album AS 'album',
+COUNT(S.usuario_id) AS 'seguidores'
+FROM SpotifyClone.artistas A
+INNER JOIN SpotifyClone.albuns AL
+ON A.id = AL.artista_id
+INNER JOIN  SpotifyClone.seguidores S 
+ON A.id = S.artista_id 
+-- GROUP BY `album`
+GROUP BY `artista`, `album`
+ORDER BY `seguidores` DESC, `artista`, `album`;
+
+-- [HONESTIDADE ACADÊMICA] dúvidas sobre quantidade de parametros para funcionar o GROUP BY solucionada
+--  seguindo orientacoes  consultadas no PR do Kyle Felipe
+
+-- Error Code: 1055. Expression #1 of SELECT list is not in GROUP BY clause and contains nonaggregated column 'spotifyclone.A.nome_artista' which is not functionally dependent on columns in GROUP BY clause; this is incompatible with sql_mode=only_full_group_by
+-- o código em DÚVIDA foi comentado na linha 11
