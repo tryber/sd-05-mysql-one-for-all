2d31eaa1aba8082693ab3cad2505e07549c50bf2
hleoc <informig@gmail.com>
2020-10-25 19:21:10 -0300

    Desafio 2 - corrigindo erros do CC

--- desafio2.sql
+++ desafio2.sql
@@ -1,6 +1,6 @@
 CREATE VIEW estatisticas_musicais AS
-	SELECT COUNT(DISTINCT C.id) AS 'cancoes',  COUNT(DISTINCT A.id) AS 'artistas', COUNT(DISTINCT AL.id) AS 'albuns' 
-    FROM SpotifyClone.cancao C,
-    SpotifyClone.artista A,
-    SpotifyClone.album AL;
+SELECT COUNT(DISTINCT C.id) AS 'cancoes',  COUNT(DISTINCT A.id) AS 'artistas', COUNT(DISTINCT AL.id) AS 'albuns' 
+FROM SpotifyClone.cancao C,
+SpotifyClone.artista A,
+SpotifyClone.album AL;
     
