33ae5c09a9fc46babda8c9fe6c202f631647e635
LizzardMedeiros <luis.medeiros@outlook.com.br>
2020-10-25 14:21:44 -0300

    CC

--- desafio8.sql
+++ desafio8.sql
@@ -5,7 +5,7 @@ CREATE TRIGGER trigger_usuario_delete
 BEFORE DELETE ON usuario
 FOR EACH ROW
 BEGIN
-	DELETE FROM reproducao WHERE usuario_id = OLD.id;
-    DELETE FROM seguindo WHERE usuario_id = OLD.id;
+  DELETE FROM reproducao WHERE usuario_id = OLD.id;
+  DELETE FROM seguindo WHERE usuario_id = OLD.id;
 END $$;
 DELIMITER ;
