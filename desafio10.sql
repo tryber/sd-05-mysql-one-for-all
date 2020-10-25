045ad178a61ae3c95dcfa39392d9f093ab98a11c
LizzardMedeiros <luis.medeiros@outlook.com.br>
2020-10-25 11:33:37 -0300

    CC

--- desafio10.sql
+++ desafio10.sql
@@ -1,10 +1,10 @@
 CREATE FUNCTION quantidade_musicas_no_historico (id SMALLINT)
 RETURNS INT READS SQL DATA
 BEGIN
-	DECLARE result INT;
-	SELECT COUNT(R.usuario_id)
-	FROM reproducao AS R
-	WHERE usuario_id = id
+  DECLARE result INT;
+  SELECT COUNT(R.usuario_id)
+  FROM reproducao AS R
+  WHERE usuario_id = id
     INTO result;
     RETURN result;
 END $$;
