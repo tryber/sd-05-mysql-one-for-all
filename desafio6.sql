3d06482ab407e3889dd6dd3b558ac2e82caba032
LizzardMedeiros <luis.medeiros@outlook.com.br>
2020-10-25 10:46:17 -0300

    one to six

---
+++ desafio6.sql
@@ -0,0 +1,8 @@
+CREATE VIEW `faturamento_atual` AS
+SELECT ROUND(MIN(P.valor), 2) AS faturamento_minimo,
+ROUND(AVG(P.valor), 2) AS faturamento_medio,
+ROUND(MAX(P.valor), 2) AS faturamento_maximo,
+ROUND(SUM(P.valor), 2) AS faturamento_total
+FROM SpotifyClone.plano as P
+INNER JOIN SpotifyClone.usuario as U
+ON U.plano_id = P.id
