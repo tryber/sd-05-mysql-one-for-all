b95757b6ed909060a331877d77ec2b4362d9b6af
Lucas Staroscky <LucasStaroscky@MacBook-Pro-de-Lucas.local>
2020-10-25 18:34:43 -0200

    corrigindo valor tabela plano - double not null

--- desafio6.sql
+++ desafio6.sql
@@ -7,3 +7,5 @@ ROUND(SUM(pl.valor), 2) AS faturamento_total
 FROM SpotifyClone.planos AS pl
 INNER JOIN SpotifyClone.usuario AS usr
 ON usr.plano_id = pl.plano_id;
+
+select * from SpotifyClone.faturamento_atual
