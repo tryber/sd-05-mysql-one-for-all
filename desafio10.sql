3cf3ac518c1656c09f17b9b437a79074a28ec487
pauloricardoz <trybe.przt@gmail.com>
2020-10-26 04:33:45 -0300

    sql10 correction in the query

--- desafio10.sql
+++ desafio10.sql
@@ -2,6 +2,7 @@ DELIMITER $$
 CREATE FUNCTION quantidade_musicas_no_historico (nome VARCHAR(100)) RETURNS INT
 BEGIN
 DECLARE saida INT;
+SET saida = 0;
 SELECT COUNT(R.usuarios_usuario_id) INTO saida
 FROM SpotifyClone.reproducao AS R
 JOIN SpotifyClone.usuarios AS U ON  R.usuarios_usuario_id = U.usuario_id
