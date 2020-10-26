f7690fd0425b114d25e4bd29e0fa265debcfdaa7
pauloricardoz <trybe.przt@gmail.com>
2020-10-26 04:49:18 -0300

    sql10 removing dispensable lines

--- desafio10.sql
+++ desafio10.sql
@@ -1,13 +1,14 @@
+USE SpotifyClone;
 DELIMITER $$
 CREATE FUNCTION quantidade_musicas_no_historico (nome VARCHAR(100)) RETURNS INT
 BEGIN
 DECLARE saida INT;
 SET saida = 0;
-SELECT COUNT(R.usuarios_usuario_id)
+SELECT COUNT(R.usuarios_usuario_id) INTO saida
 FROM SpotifyClone.reproducao AS R
 JOIN SpotifyClone.usuarios AS U ON  R.usuarios_usuario_id = U.usuario_id
 WHERE nome=U.usuario
-GROUP BY usuario INTO saida;
+GROUP BY usuario;
 RETURN saida;
 END $$
 DELIMITER ;
