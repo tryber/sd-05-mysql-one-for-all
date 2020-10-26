1a825e6271476207be09d3a0ec7c2148bbad1b5d
pauloricardoz <trybe.przt@gmail.com>
2020-10-26 04:39:08 -0300

    sql10 correction in the query. Moving the INTO to the end

--- desafio10.sql
+++ desafio10.sql
@@ -3,11 +3,11 @@ CREATE FUNCTION quantidade_musicas_no_historico (nome VARCHAR(100)) RETURNS INT
 BEGIN
 DECLARE saida INT;
 SET saida = 0;
-SELECT COUNT(R.usuarios_usuario_id) INTO saida
+SELECT COUNT(R.usuarios_usuario_id)
 FROM SpotifyClone.reproducao AS R
 JOIN SpotifyClone.usuarios AS U ON  R.usuarios_usuario_id = U.usuario_id
 WHERE nome=U.usuario
-GROUP BY usuario;
+GROUP BY usuario INTO saida;
 RETURN saida;
 END $$
 DELIMITER ;
