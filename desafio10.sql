ff5bc96bcf5fad304c9dce63a175501c5247c715
pauloricardoz <trybe.przt@gmail.com>
2020-10-26 04:55:11 -0300

    sql10 deterministic

--- desafio10.sql
+++ desafio10.sql
@@ -1,6 +1,7 @@
-USE SpotifyClone;
 DELIMITER $$
-CREATE FUNCTION quantidade_musicas_no_historico (nome VARCHAR(100)) RETURNS INT
+CREATE FUNCTION quantidade_musicas_no_historico (nome VARCHAR(100))
+RETURNS INT
+DETERMINISTIC
 BEGIN
 DECLARE saida INT;
 SET saida = 0;
@@ -9,6 +10,6 @@ FROM SpotifyClone.reproducao AS R
 JOIN SpotifyClone.usuarios AS U ON  R.usuarios_usuario_id = U.usuario_id
 WHERE nome=U.usuario
 GROUP BY usuario;
-RETURN saida;
+RETURN (saida);
 END $$
 DELIMITER ;
