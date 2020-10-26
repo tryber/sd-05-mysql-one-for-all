8f9ac91e7c7dd1ae864ff8c83f5758d4a45e2b6c
pauloricardoz <trybe.przt@gmail.com>
2020-10-26 05:06:23 -0300

    sql10 changed user_name for user_id

--- desafio10.sql
+++ desafio10.sql
@@ -8,7 +8,7 @@ SET saida = 0;
 SELECT COUNT(R.usuarios_usuario_id) INTO saida
 FROM SpotifyClone.reproducao AS R
 JOIN SpotifyClone.usuarios AS U ON  R.usuarios_usuario_id = U.usuario_id
-WHERE nome=U.usuario
+WHERE nome=U.usuario_id
 GROUP BY usuario;
 RETURN (saida);
 END $$
