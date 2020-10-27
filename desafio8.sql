12b323cb48b69f9bd5f773b89ec2e91bc1bbf27a
Mau <mauricio.sdjr@gmail.com>
2020-10-27 10:31:44 -0300

    desafio 8

---
+++ desafio8.sql
@@ -0,0 +1,9 @@
+DELIMITER $$
+CREATE TRIGGER trigger_usuario_delete
+AFTER DELETE ON usuarios
+FOR EACH ROW
+BEGIN
+DELETE FROM seguindo_artistas WHERE usuario_id = OLD.usuario_id;
+DELETE FROM historio_de_reproducoes WHERE usuario_id = OLD.usuario_id;
+END; $$
+DELIMITER ;
