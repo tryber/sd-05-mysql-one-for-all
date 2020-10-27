e0ac13c50df4de6a836ad889efa8397ab1bb08ea
Mau <mauricio.sdjr@gmail.com>
2020-10-27 10:49:12 -0300

    corrigindo desafio 8

--- desafio8.sql
+++ desafio8.sql
@@ -1,9 +1,9 @@
 DELIMITER $$
 CREATE TRIGGER trigger_usuario_delete
-AFTER DELETE ON usuarios
+BEFORE DELETE ON usuarios
 FOR EACH ROW
 BEGIN
 DELETE FROM seguindo_artistas WHERE usuario_id = OLD.usuario_id;
 DELETE FROM historico_de_reproducoes WHERE usuario_id = OLD.usuario_id;
-END; $$
+END $$
 DELIMITER ;
