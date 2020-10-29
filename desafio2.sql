29819e2882a01507ec76a0d227a563c62a670c92
Eder Sena <eder.sena@live.com>
2020-10-29 18:42:56 -0200

    desafio 5

--- desafio2.sql
+++ desafio2.sql
@@ -1,6 +1,6 @@
 CREATE VIEW estatisticas_musicais AS
 SELECT
-COUNT(DISTINCT c.name) AS 'cancoes',
-COUNT(DISTINCT at.name) AS 'artistas',
-COUNT(DISTINCT ab.name) AS 'albuns'
+COUNT(DISTINCT c.name) AS cancoes,
+COUNT(DISTINCT at.name) AS artistas,
+COUNT(DISTINCT ab.name) AS albuns
 FROM SpotifyClone.album as ab, SpotifyClone.artista as at, SpotifyClone.cancoes as c;
--- desafio3.sql
+++ desafio3.sql
@@ -1,8 +1,8 @@
 CREATE VIEW historico_reproducao_usuarios AS
 SELECT
-u.name AS `usuario`,
-c.name AS `nome`
+u.name AS usuario,
+c.name AS nome
 FROM SpotifyClone.historico_de_reproducoes h
 INNER JOIN SpotifyClone.usuario u ON h.usuario_id = u.usuario_id
 INNER JOIN SpotifyClone.cancoes c ON h.cancoes_id = c.cancoes_id
-ORDER BY `usuario`, `nome`;
+ORDER BY usuario, nome;
