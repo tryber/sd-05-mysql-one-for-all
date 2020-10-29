bb93c649c9eb5ca0b35f1601332cd9e584ecc0cf
Eder Sena <eder.sena@live.com>
2020-10-29 19:25:57 -0200

    Update desafio5.sql

--- desafio5.sql
+++ desafio5.sql
@@ -1,7 +1,9 @@
 CREATE VIEW top_2_hits_do_momento AS
 SELECT
 c.name as 'cancao',
-(SELECT COUNT(*) FROM SpotifyClone.historico_de_reproducoes AS h WHERE c.cancoes_id = h.cancoes_id) AS 'reproducoes'
-from SpotifyClone.cancoes as c
+COUNT(*) as 'reproducoes'
+FROM SpotifyClone.cancoes as c
+JOIN SpotifyClone.historico_de_reproducoes AS h
+ON c.cancoes_id = h.cancoes_id
 GROUP BY `cancao`
 ORDER BY `reproducoes` DESC, `cancao` LIMIT 2;
