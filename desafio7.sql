4780708dc0ceb33e516e293f44df95c092661db7
Eder Sena <eder.sena@live.com>
2020-10-29 20:23:33 -0200

    Update desafio7.sql

--- desafio7.sql
+++ desafio7.sql
@@ -1,9 +1,9 @@
 CREATE VIEW perfil_artistas AS
 SELECT
-a.name as artista,
-b.name as album,
+A.name as artista,
+AL.name as album,
 COUNT(*) AS seguidores
-FROM SpotifyClone.seguindo_artistas AS s, SpotifyClone.artista as a, SpotifyClone.album AS b
-WHERE s.artista_id = a.artista_id AND a.artista_id = b.artista_id
+FROM SpotifyClone.seguindo_artistas AS S, SpotifyClone.artista as A, SpotifyClone.album AS AL
+WHERE S.artista_id = A.artista_id AND A.artista_id = AL.artista_id
 GROUP BY artista, album
 ORDER BY seguidores DESC, artista, album;
