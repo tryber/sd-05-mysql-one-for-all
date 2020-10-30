f0555e6c66a26dcf073cf12bb9f6d80915b183bd
Marylange <marylangess@gmail.com>
2020-10-30 01:25:38 -0300

    terceiro requisito

--- desafio2.sql
+++ desafio2.sql
@@ -1,6 +1,5 @@
-USE SpotifyClone;
 CREATE VIEW estatisticas_musicais AS
-	SELECT COUNT(DISTINCT c.CANCOES_ID) AS 'cancoes',
-	COUNT(DISTINCT a.ARTISTA_ID) AS 'artistas',
-	COUNT(DISTINCT al.ALBUM_ID) AS 'albuns'
-	FROM cancoes AS c, artista AS a, album AS al;
+  SELECT COUNT(DISTINCT c.CANCOES_ID) AS 'cancoes',
+  COUNT(DISTINCT a.ARTISTA_ID) AS 'artistas',
+  COUNT(DISTINCT al.ALBUM_ID) AS 'albuns'
+  FROM SpotifyClone.cancoes AS c, SpotifyClone.artista AS a, SpotifyClone.album AS al;
