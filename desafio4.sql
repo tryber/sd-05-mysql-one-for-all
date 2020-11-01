8f5b8cbac9088c82bb0c456d5e9db6ed088d126e
Alexander Mitchell <alexlm.trybe@gmail.com>
2020-11-01 11:30:20 -0300

    depois de finalmente corrigir o desafio 1 passou em tudo menos o desafio4, adicionei a ORDER BY artista e passou

--- desafio4.sql
+++ desafio4.sql
@@ -11,5 +11,5 @@ CREATE VIEW top_3_artistas AS
 SELECT a.artista AS artista,
 (SELECT COUNT(*) FROM SpotifyClone.seguindo_artistas AS sa WHERE a.artista_id = sa.artista_id) AS seguidores 
 FROM SpotifyClone.artistas AS a
-ORDER BY seguidores DESC
+ORDER BY seguidores DESC, artista
 LIMIT 3;
