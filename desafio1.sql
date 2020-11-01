5ecaf63b3db2c2f8f7e67ac7cfb7410f3f37d8ff
Alexander Mitchell <alexlm.trybe@gmail.com>
2020-11-01 10:27:22 -0300

    desafio1.json e sql

---
+++ desafio1.sql
@@ -0,0 +1,149 @@
+DROP DATABASE IF EXISTS SpotifyClone;
+
+CREATE DATABASE SpotifyClone;
+
+USE SpotifyClone;
+
+CREATE TABLE `SpotifyClone`.`planos` (
+  `plano_id` INT UNIQUE AUTO_INCREMENT NOT NULL,,
+  `plano` VARCHAR(45) NOT NULL,
+  `valor_plano` DECIMAL(5,2) NOT NULL,
+  PRIMARY KEY (`plano_id`))
+ENGINE = InnoDB;
+
+CREATE TABLE `SpotifyClone`.`usuarios` (
+  `usuario_id` INT UNIQUE AUTO_INCREMENT NOT NULL,
+  `usuario` VARCHAR(45) NOT NULL,
+  `idade` INT NOT NULL,
+  `plano_id` INT NOT NULL,
+  PRIMARY KEY (`usuario_id`),
+  CONSTRAINT `fk_plano_id`
+    FOREIGN KEY (`plano_id`)
+    REFERENCES `SpotifyClone`.`planos` (`plano_id`)
+) ENGINE = InnoDB;
+
+CREATE TABLE `SpotifyClone`.`artistas` (
+  `artista_id` INT UNIQUE AUTO_INCREMENT NOT NULL,
+  `artista` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`artista_id`))
+ENGINE = InnoDB;
+
+CREATE TABLE `SpotifyClone`.`albuns` (
+  `album_id` INT UNIQUE AUTO_INCREMENT NOT NULL,
+  `album` VARCHAR(45) NOT NULL,
+  `artista_id` INT NOT NULL,
+  PRIMARY KEY (`album_id`),
+  CONSTRAINT `fk_albuns_artista_id`
+    FOREIGN KEY (`artista_id`)
+    REFERENCES `SpotifyClone`.`artistas` (`artista_id`)
+) ENGINE = InnoDB;
+
+CREATE TABLE `SpotifyClone`.`cancoes` (
+  `cancao_id` INT UNIQUE AUTO_INCREMENT NOT NULL,
+  `cancao` VARCHAR(45) NOT NULL,
+  `album_id` INT NOT NULL,
+  PRIMARY KEY (`cancao_id`),
+  UNIQUE INDEX `cancao_UNIQUE` (`cancao` ASC) VISIBLE,
+  CONSTRAINT `fk_cancoes_album_id`
+    FOREIGN KEY (`album_id`)
+    REFERENCES `SpotifyClone`.`albuns` (`album_id`)
+) ENGINE = InnoDB;
+
+CREATE TABLE `SpotifyClone`.`historico_de_reproducoes` (
+  `usuario_id` INT NOT NULL,
+  `cancao_id` INT NOT NULL,
+  CONSTRAINT `fk_hdr_usuario_id`
+    FOREIGN KEY (`usuario_id`)
+    REFERENCES `SpotifyClone`.`usuarios` (`usuario_id`),
+  CONSTRAINT `fk_hdr_cancao_id`
+    FOREIGN KEY (`cancao_id`)
+    REFERENCES `SpotifyClone`.`cancoes` (`cancao_id`)
+) ENGINE = InnoDB;
+
+CREATE TABLE `SpotifyClone`.`seguindo_artistas` (
+  `usuario_id` INT NOT NULL,
+  `artista_id` INT NOT NULL,
+  CONSTRAINT `fk_sa_usuario_id`
+    FOREIGN KEY (`usuario_id`)
+    REFERENCES `SpotifyClone`.`usuarios` (`usuario_id`),
+  CONSTRAINT `fk_sa_artista_id`
+    FOREIGN KEY (`artista_id`)
+    REFERENCES `SpotifyClone`.`artistas` (`artista_id`)
+) ENGINE = InnoDB;
+
+INSERT INTO planos (plano, valor_plano)
+VALUES
+('gratuito', 0.00),
+('universitário', 5.99),
+('familiar', 7.99);
+
+INSERT INTO usuarios (usuario, idade, plano_id)
+VALUES
+('Thati', 23, 1),
+('Cintia', 35, 3),
+('Bill', 20, 2),
+('Roger', 45, 1);
+
+INSERT INTO artistas (artista)
+VALUES
+('Walter Phoenix'),
+('Peter Strong'),
+('Lance Day'),
+('Freedie Shannon');
+
+INSERT INTO albuns (album, artista_id)
+VALUES
+('Envious', 1),
+('Exuberant', 1),
+('Hallowed Steam', 2),
+('Incandescent', 3),
+('Temporary Culture', 4);
+
+INSERT INTO cancoes (cancao, album_id)
+VALUES
+('Soul For Us', 1),
+('Reflections Of Magic', 1),
+('Dance With Her Own', 1),
+('Troubles Of My Inner Fire', 2),
+('Time Fireworks', 2),
+('Magic Circus', 3),
+('Honey, So Do I', 3),
+("Sweetie, Let's Go Wild", 3),
+('She Knows', 3),
+('Fantasy For Me', 4),
+('Celebration Of More', 4),
+('Rock His Everything', 4),
+('Home Forever', 4),
+('Diamond Power', 4),
+("Honey, Let's Be Silly", 4),
+('Thang Of Thunder', 5),
+('Words Of Her Life', 5),
+('Without My Streets', 5);
+
+INSERT INTO historico_de_reproducoes (usuario_id, cancao_id)
+VALUES
+(1, 1),
+(1, 6),
+(1, 14),
+(1, 16),
+(2, 13),
+(2, 17),
+(2, 2),
+(2, 15),
+(3, 4),
+(3, 16),
+(3, 6),
+(4, 3),
+(4, 18),
+(4, 11);
+
+INSERT INTO seguindo_artistas (usuario_id, artista_id)
+VALUES
+(1, 1),
+(1, 4),
+(1, 3),
+(2, 1),
+(2, 3),
+(3, 2),
+(3, 1),
+(4, 4);
