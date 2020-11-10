cf9256fc33e0501eebdf66e5e4967088b001c47f
luialbeto <araujo.script@gmail.com>
2020-11-09 22:36:19 -0300

    insert

--- desafio1.sql
+++ desafio1.sql
@@ -1,5 +1,8 @@
-CREATE SCHEMA IF NOT EXISTS `SpotifyClone` ;
-USE `SpotifyClone` ;
+DROP DATABASE IF EXISTS SpotifyClone;
+
+CREATE SCHEMA IF NOT EXISTS `SpotifyClone`;
+
+USE `SpotifyClone`;
 
 CREATE TABLE IF NOT EXISTS `SpotifyClone`.`plano` (
   `id` INT NOT NULL AUTO_INCREMENT,
@@ -29,7 +32,6 @@ CREATE TABLE IF NOT EXISTS `SpotifyClone`.`artista` (
 )
 ENGINE = InnoDB;
 
-
 CREATE TABLE IF NOT EXISTS `SpotifyClone`.`album` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `nome_album` VARCHAR(45) NULL,
@@ -47,8 +49,6 @@ CREATE TABLE IF NOT EXISTS `SpotifyClone`.`cancoes` (
   `nome_cancoes` VARCHAR(45) NULL,
   `album_id` INT NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,
-  INDEX `fk_cancoes_album1_idx` (`album_id` ASC) VISIBLE,
   CONSTRAINT `fk_cancoes_album1`
     FOREIGN KEY (`album_id`)
     REFERENCES `SpotifyClone`.`album` (`id`)
@@ -89,3 +89,80 @@ CREATE TABLE IF NOT EXISTS `SpotifyClone`.`usuario_has_artista` (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
+
+INSERT INTO plano (plano, valor_plano)
+VALUES
+  ('gratuito', 0.00),
+  ('familiar', 7.99),
+  ('universitário', 5.99);
+
+INSERT INTO usuario (usuarios, idade, planos_id)
+VALUES
+  ('Thati', 23, 1),
+  ('Cintia', 35, 2),
+  ('Bill', 20, 3),
+  ('Roger', 45, 1);
+
+INSERT INTO artista (artista)
+VALUES
+  ('Walter Phoenix'),
+  ('Peter Strong'),
+  ('Lance Day'),
+  ('Freedie Shannon');
+  
+INSERT INTO album (nome_album, artistas_id)
+VALUES
+  ('Envious', 1),
+  ('Exuberant', 1),
+  ('Hallowed Steam', 2),
+  ('Incandescent', 3),
+  ('Temporary Culture', 4);
+
+INSERT INTO cancoes (nome_cancoes, album_id)
+VALUES
+  ('Soul For Us', 1),
+  ('Reflections Of Magic', 1),
+  ('Dance With Her Own', 1),
+  ('Troubles Of My Inner Fire', 2),
+  ('Time Fireworks', 2),
+  ('Magic Circus', 3),
+  ('Honey, So Do I', 3),
+  ("Sweetie, Let's Go Wild", 3),
+  ('She Knows', 3),
+  ('Fantasy For Me', 4),
+  ('Celebration Of More', 4),
+  ('Rock His Everything', 4),
+  ('Home Forever', 4),
+  ('Diamond Power', 4),
+  ("Honey, Let's Be Silly", 4),
+  ('Thang Of Thunder', 5),
+  ('Words Of Her Life', 5),
+  ('Without My Streets', 5);
+
+INSERT INTO usuario_has_cancoes (usuario_id, cancoes_id)
+VALUES
+  (1, 1),
+  (1, 6),
+  (1, 14),
+  (1, 16),
+  (2, 13),
+  (2, 17),
+  (2, 2),
+  (2, 15),
+  (3, 4),
+  (3, 16),
+  (3, 6),
+  (4, 3),
+  (4, 18),
+  (4, 11);
+
+INSERT INTO usuario_has_artista (usuario_id, artista_id)
+VALUES
+  (1, 1),
+  (1, 4),
+  (1, 3),
+  (2, 1),
+  (2, 3),
+  (3, 2),
+  (3, 1),
+  (4, 4);
