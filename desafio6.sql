c9afe17d82211c14e84c605cb1c1a4e5b800f70a
luialbeto <araujo.script@gmail.com>
2020-11-15 12:54:21 -0300

    corrigindo1 desafio 6

--- desafio6.sql
+++ desafio6.sql
@@ -5,6 +5,6 @@ ROUND(MAX(p.valor_plano), 2) AS faturamento_maximo,
 ROUND(AVG(p.valor_plano), 2) AS faturamento_medio,
 ROUND(SUM(p.valor_plano), 2) AS faturamento_total
 FROM
-SpotifyClone.planos AS P
+SpotifyClone.planos AS p
 INNER JOIN SpotifyClone.usuarios AS u
 ON u.planos_id = p.id;
