37bb201c7d8cd22955ab4087c032056b8e65af04
Giuliano Peixoto <giulianopeixoto@gmail.com>
2020-11-15 13:25:31 -0300

    CC

--- desafio10.sql
+++ desafio10.sql
@@ -2,12 +2,12 @@ DELIMITER $$
 CREATE FUNCTION quantidade_musicas_no_historico(id INT)
 RETURNS INT READS SQL DATA
 BEGIN
-	DECLARE qtdeRep INT;
-	SELECT
-	COUNT(cancao_id)
-	INTO qtdeRep
-	FROM SpotifyClone.historico_reproducao
-	WHERE usuario_id = id;
-	RETURN qtdeRep;
+DECLARE qtdeRep INT;
+SELECT
+COUNT(cancao_id)
+INTO qtdeRep
+FROM SpotifyClone.historico_reproducao
+WHERE usuario_id = id;
+RETURN qtdeRep;
 END $$
 DELIMITER ;
