aef9e93db62a5a0ef741905e54a1a0cf7af2eec3
luialbeto <araujo.script@gmail.com>
2020-11-15 12:21:42 -0300

    corrigindo1 desafio 4

--- desafio4.sql
+++ desafio4.sql
@@ -7,4 +7,5 @@ SpotifyClone.artistas AS art
 INNER JOIN SpotifyClone.seguindo_artistas AS s
 ON art.id = s.artistas_id
 GROUP BY art.artista, s.artistas_id
-ORDER BY 2 DESC, 1;
+ORDER BY 2 DESC, 1
+LIMIT 3;
