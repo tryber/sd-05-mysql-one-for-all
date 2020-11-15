5c1d3c11ee179564e197da3fa3dd740b17440e66
luialbeto <araujo.script@gmail.com>
2020-11-14 22:35:41 -0300

    desafio 3

---
+++ desafio3.sql
@@ -0,0 +1,11 @@
+CREATE VIEW historico_reproducao_usuarios AS
+SELECT
+u.usuario AS usuario
+c.cancao AS nome
+FROM
+SpotifyClone.usuarios AS u
+INNER JOIN SpotifyClone.historico_reproducoes hr
+ON hr.usuarios_id = u.id
+INNER JOIN SpotifyClone.cancoes AS c
+ON hr.cancoes_id = c.id
+ORDER BY u.usuario, c.cancao
