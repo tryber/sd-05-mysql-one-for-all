43fb64af4d8487ac88a4ec161139d18eaa0eedd4
luialbeto <araujo.script@gmail.com>
2020-11-14 23:08:08 -0300

    corrigindo erro na sintaxe

--- desafio3.sql
+++ desafio3.sql
@@ -4,8 +4,8 @@ u.usuario AS usuario
 c.cancao AS nome
 FROM
 SpotifyClone.usuarios AS u
-INNER JOIN SpotifyClone.historico_reproducoes hr
+INNER JOIN SpotifyClone.historico_reproducoes AS hr
 ON hr.usuarios_id = u.id
 INNER JOIN SpotifyClone.cancoes AS c
 ON hr.cancoes_id = c.id
-ORDER BY u.usuario, c.cancao;
+ORDER BY 1, 2;
