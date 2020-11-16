df6e66eafa30c4eed42b080c64b1fb3b6da7e331
luialbeto <araujo.script@gmail.com>
2020-11-15 23:07:02 -0300

    desafio 7

---
+++ desafio7.sql
@@ -0,0 +1,12 @@
+CREATE VIEW perfil_artistas AS
+SELECT
+art.artista AS artista
+a.album AS album,
+COUNT(*) AS seguidores
+FROM SpotifyClone.artistas AS art
+INNER JOIN SpotifyClone.albuns AS a
+ON a.artistas_id = art.id
+INNER JOIN SpotifyClone.seguindo_artistas AS s
+ON s.artistas_id = art.id
+GROUP BY artista, album
+ORDER BY seguidores DESC, artista, album;
