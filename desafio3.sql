1841e5c46b69bc9ec876e3eaca4b3a283809f087
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2021-01-05 09:54:55 -0300

    fixing misspelled

--- desafio3.sql
+++ desafio3.sql
@@ -2,6 +2,6 @@ CREATE VIEW historico_reproducao_usuarios AS
 SELECT usr.usuario AS usuario, c.cancao AS nome
 FROM SpotifyClone.usuarios AS user
 INNER JOIN SpotifyClone.cancoes AS cancoes
-INNER JOIN SpotifyClone.historico_de_reproducoes AS historioco
+INNER JOIN SpotifyClone.historico_de_reproducoes AS historico
 ON historico.cancao_id = cancoes.cancao_id AND historico.usuario_id = user.usuario_id
 ORDER BY usuario, nome;
